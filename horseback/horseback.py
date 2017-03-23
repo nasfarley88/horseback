214235bcf6a65cc9817eb3d6286d9d21072be03c
Nathanael Farley <nasfarley88@gmail.com>
2017-03-23 10:50:02 +0000

    Added more docs and exceptions.

--- horseback/horseback.py
+++ horseback/horseback.py
@@ -1,6 +1,31 @@
+class ServiceNotFound(Exception):
+    pass
+
+
+class ServicesDict(dict):
+    """Dictionary of services.
+
+    Only differs by raising a ServiceNotFound error instead of KeyError if key
+    is not found.
+
+    """
+
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except KeyError:
+            raise ServiceNotFound("Service '{}' not found.".format(key))
+
+
 class Horseback:
+    """Class used to interact with `ChatService`s.
+
+    Horseback takes a list of `ChatService`s and uses them to interact with the
+    service. If a service is not found, a ServiceNotFound exception is raised.
+
+    """
     def __init__(self, services):
-        self.services = {s.service_type: s for s in services}
+        self.services = ServicesDict({s.service_type: s for s in services})
 
     async def send_text_message(self, service_type, chat_id, text, **kwargs):
         service = self.services[service_type]
