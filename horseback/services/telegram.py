8e7ba28a2f0bca7c5e3acb953f77ad4d2f50cd58
Nathanael Farley <nasfarley88@gmail.com>
2017-03-24 21:16:42 +0000

    Restructured telegram service to use aiohttp.

--- horseback/services/telegram.py
+++ horseback/services/telegram.py
@@ -1,20 +1,33 @@
 from telepot import Bot
 
-from ..chatobjects import TextMessage
+from ..chatobjects.textmessage import TextMessage
 
 
 class Telegram:
-    def __init__(self, key, *args, **kwargs):
+    def __init__(self, session, key, *args, **kwargs):
         "Basic bot for Telegram sending messages"
+        self.session = session
+        self.key = key
+
         self._bot = Bot(key)
         self.service_type = "telegram"
 
     async def send_text_message(self, chat_id, text, **kwargs) -> TextMessage:
         # TODO figure out how to make this async
-        json = self._bot.sendMessage(chat_id, text, **kwargs)
+        # json = self._bot.sendMessage(chat_id, text, **kwargs)
+        payload = {
+            'chat_id': chat_id,
+            'text': text,
+            **kwargs
+        }
+        async with self.session.post(
+                "https://api.telegram.org/bot{}/sendMessage".format(
+                    self.key), data=payload) as resp:
+            json = await resp.json()
+
         return TextMessage(
             json,
-            json['chat_id'],
-            json['message_id'],
-            json['text'],
+            json['result']['chat']['id'],
+            json['result']['message_id'],
+            json['result']['text'],
         )
--- tests/test_telegram.py
+++ tests/test_telegram.py
@@ -1,5 +1,6 @@
 import unittest
 import asyncio
+import aiohttp
 import yaml
 
 from horseback.services.telegram import Telegram
@@ -12,14 +13,20 @@ class TestTelegramBot(unittest.TestCase):
         asyncio.set_event_loop(self.loop)
 
         self.config = yaml.load(open("test_config.yml"))
-        self.bot = Telegram(self.config['telegram_key'])
-        self.hb = Horseback([self.bot])
 
     def test_send_text_message(self):
         async def foo():
-            await self.hb.send_text_message(
-                "telegram",
-                self.config['telegram_chat_id'],
-                self.config['sample_text'])
+            # Set up the aiohttp session
+            async with aiohttp.ClientSession() as session:
+
+                # Set up the telegram service and horseback object
+                self.bot = Telegram(session, self.config['telegram_key'])
+                self.hb = Horseback([self.bot])
+
+                # Send message
+                await self.hb.send_text_message(
+                    "telegram",
+                    self.config['telegram_chat_id'],
+                    self.config['sample_text'])
 
         self.loop.run_until_complete(foo())
